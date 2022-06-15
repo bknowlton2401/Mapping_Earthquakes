b6563e7c6333f6947a45d43c366282d14e37b801
Bethany Knowlton <bknowlton22@gmail.com>
2022-06-15 13:21:04 -0700

    update geojson points

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// We create the light view tile layer that will be the background of our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the streets view tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -21,18 +21,15 @@ let baseMaps = {
     Dark: dark
   };
 
-  // Create the map object with center, zoom level and default layer.
+// Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
+    center: [30, 30],
     zoom: 2,
-    layers: [light]
+    layers: [streets]
 })
-
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
-
-// Then we add our 'light' tile layer to the map.
-light.addTo(map);
+  
+// Then we add our 'streets' tile layer to the map.
+streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/bknowlton2401/Mapping_Earthquakes/main/majorAirports.json";
